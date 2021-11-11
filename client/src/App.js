3bb7c4b5efb1a94262916a5e5d370926ddff3af2
Stella Oseyomon <stella.oseyomon@gmail.com>
2021-11-11 19:37:21 +0100

    register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
+import Signup from "./pages/Signup";
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+        <BrowserRouter>
+          <div className = 'content' basename="/React">
+            <Routes>
+              <Route exact path = '/' element = {<Home />} />
+              <Route exact path = '/signup' element = {<Signup />} />
+
+            </Routes>
+          </div>
+        </BrowserRouter>
     </div>
   );
 }
