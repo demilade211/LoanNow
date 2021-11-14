86e0ce026960b45d8531e0897c5b7e10605a7214
Stella Oseyomon <stella.oseyomon@gmail.com>
2021-11-14 22:25:13 +0100

    removed signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 import Home from './pages/Home';
-import Signup from "./pages/Signup";
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
           <div className = 'content' basename="/React">
             <Routes>
               <Route exact path = '/' element = {<Home />} />
-              <Route exact path = '/signup' element = {<Signup />} />
+              <Route exact path = '/dashboard' element = {<Dashboard />} />
 
             </Routes>
           </div>
