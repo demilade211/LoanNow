6273594448a5329f9006ecc7dd9e6f0b43651d0b
demilade211 <afolabiogunbanwo@gmail.com>
2021-11-25 10:55:41 +0100

    added forgot password route

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,11 +1,14 @@
 import  express from "express";
-import { registerUser,loginUser, logOut,getUserProfile } from "../controllers/authController";
+import { registerUser,loginUser, logOut,getUserProfile,forgotPassword,resetPassword } from "../controllers/authController";
 import {authenticateUser} from "../Middlewares/authMiddleware"
 
 const router = express.Router()
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+
+router.route('/password/forgot').post(forgotPassword);
+router.route('/password/reset/:token').put(resetPassword);
 
 router.route('/me').get(authenticateUser,getUserProfile);
 
